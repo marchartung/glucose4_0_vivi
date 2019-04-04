e482578101d7c3461777ade2defd8616f8ddba3f
bzchartu <hartung@zib.de>
2019-04-04 22:27:56 +0200

    init commit

---
+++ parallel/depend.mk
@@ -0,0 +1,143 @@
+/home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/MultiSolvers.o /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/MultiSolvers.or /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/MultiSolvers.od /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/MultiSolvers.op: \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/MultiSolvers.cc \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../parallel/MultiSolvers.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../parallel/ParallelSolver.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../core/SolverTypes.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/IntTypes.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/Alg.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/Vec.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/XAlloc.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/Map.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/Alloc.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../core/Solver.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/Heap.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../utils/Options.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../utils/ParseUtils.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../core/BoundedQueue.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../core/Constants.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/Clone.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../simp/SimpSolver.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/Queue.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../parallel/SharedCompanion.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../parallel/SolverCompanion.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../parallel/ClausesBuffer.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/Sort.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../utils/System.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../parallel/SolverConfiguration.h
+/home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/SharedCompanion.o /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/SharedCompanion.or /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/SharedCompanion.od /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/SharedCompanion.op: \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/SharedCompanion.cc \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../core/Solver.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/Heap.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/Vec.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/IntTypes.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/XAlloc.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/Alg.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../utils/Options.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../utils/ParseUtils.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../core/SolverTypes.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/Map.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/Alloc.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../core/BoundedQueue.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../core/Constants.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/Clone.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../parallel/ParallelSolver.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../simp/SimpSolver.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/Queue.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../parallel/SharedCompanion.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../parallel/SolverCompanion.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../parallel/ClausesBuffer.h
+/home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/ClausesBuffer.o /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/ClausesBuffer.or /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/ClausesBuffer.od /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/ClausesBuffer.op: \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/ClausesBuffer.cc \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../parallel/ClausesBuffer.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/Vec.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/IntTypes.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/XAlloc.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../core/SolverTypes.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/Alg.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/Map.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/Alloc.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../core/Solver.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/Heap.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../utils/Options.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../utils/ParseUtils.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../core/BoundedQueue.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../core/Constants.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/Clone.h
+/home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/ParallelSolver.o /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/ParallelSolver.or /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/ParallelSolver.od /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/ParallelSolver.op: \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/ParallelSolver.cc \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../parallel/ParallelSolver.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../core/SolverTypes.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/IntTypes.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/Alg.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/Vec.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/XAlloc.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/Map.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/Alloc.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../core/Solver.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/Heap.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../utils/Options.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../utils/ParseUtils.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../core/BoundedQueue.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../core/Constants.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/Clone.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../simp/SimpSolver.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/Queue.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../parallel/SharedCompanion.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../parallel/SolverCompanion.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../parallel/ClausesBuffer.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/Sort.h
+/home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/SolverCompanion.o /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/SolverCompanion.or /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/SolverCompanion.od /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/SolverCompanion.op: \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/SolverCompanion.cc \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../parallel/SolverCompanion.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/Vec.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/IntTypes.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/XAlloc.h
+/home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/SolverConfiguration.o /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/SolverConfiguration.or /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/SolverConfiguration.od /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/SolverConfiguration.op: \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/SolverConfiguration.cc \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../parallel/MultiSolvers.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../parallel/ParallelSolver.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../core/SolverTypes.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/IntTypes.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/Alg.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/Vec.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/XAlloc.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/Map.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/Alloc.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../core/Solver.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/Heap.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../utils/Options.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../utils/ParseUtils.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../core/BoundedQueue.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../core/Constants.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/Clone.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../simp/SimpSolver.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/Queue.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../parallel/SharedCompanion.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../parallel/SolverCompanion.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../parallel/ClausesBuffer.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../parallel/SolverConfiguration.h
+/home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/Main.o /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/Main.or /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/Main.od /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/Main.op: \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/Main.cc \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../utils/System.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/IntTypes.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../utils/ParseUtils.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../utils/Options.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/Vec.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/XAlloc.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../core/Dimacs.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../core/SolverTypes.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/Alg.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/Map.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/Alloc.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../simp/SimpSolver.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/Queue.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../core/Solver.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/Heap.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../core/BoundedQueue.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../core/Constants.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../mtl/Clone.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../parallel/ParallelSolver.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../parallel/SharedCompanion.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../parallel/SolverCompanion.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../parallel/ClausesBuffer.h \
+ /home/hartung/HPSV/programs/glucose-syrup-4.0_vivi/parallel/../parallel/MultiSolvers.h
